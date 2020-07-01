d9c95ed1431057dfba98c898bac867eee25d657a
HungNX <hung10121997@gmail.com>
2020-07-01 22:28:33 +0700

    code base

---
+++ SeaFood/SeaFood/SeaFood.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaFood.Core
+{
+    class Class1
+    {
+    }
+}
---
+++ SeaFood/SeaFood/SeaFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeaFood.Startup))]
+namespace SeaFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
